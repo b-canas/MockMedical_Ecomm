4874841781fb173c4aec2ce39e2dbce61a41d711
Void-Star-Coding <62954238+Void-Star-Coding@users.noreply.github.com>
2021-03-18 14:19:56 -0400

    Updated index.php and loginstyle.css
    Added navbar, highlights links red on hover
    change background color to very light gray
    button is blue but can't figure out how to do that in the css file

--- Vitamin Project/index.php
+++ Vitamin Project/index.php
@@ -1,31 +1,42 @@
-<!DOCTYPE html>
+
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="loginstyle.css">
-    <title>Medicine Site</title>
-</head>
-<body>
-    <?php
-    include 'dbConnection.php';
-    ?>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="loginstyle.css">
+		<title>VitiMins - Login Page</title>
+	</head>
+	<body>
+		<?php
+		include 'dbConnection.php';
+		?>
+		<p>
+		<div class="nav">
+			
+			<div class="nav-title">
+				<li>VitiMins</li>
+			</div>
+			
+			<div class="nav-links">
+				<li><a href="home.php">Home</a></li>
+				<li><a href="products.php">Products</a></li>
+			</div>
+			
+		</div>
+		
+		<div class="center">
+			<h1>Login</h1>
 
-    
-    <div class="center">
+			<label for="username"></label>
+			<input type="text" placeholder="Username" name="username"><br>
 
-        <h1>Login</h1>
+			<label for="password"></label>
+			<input type="text" placeholder="Password" name="password"><br>
 
-        <label for="username"></label>
-        <input type="text" placeholder="Username" name="username"><br>
+			<button type="submit" style="background-color: 008CBA;">Login</button>
 
-        <label for="password"></label>
-        <input type="text" placeholder="Password" name="password"><br>
+		</div>
 
-        <button type="submit">Login</button>
-
-    </div>
-
-    
-</body>
+		
+	</body>
 </html>
