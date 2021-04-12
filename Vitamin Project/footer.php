dca68490393a4e60c8a785f841649580a581e487
b-canas <78327376+b-canas@users.noreply.github.com>
2021-04-12 17:59:14 -0400

    Add footer file via upload
    This footer file is just the isolated footer Walter had designed in his index.html. This allows us to easily include the footer in all necessary webpages.

---
+++ Vitamin Project/footer.php
@@ -0,0 +1,14 @@
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <p>Our Purpose Is To Distribute Vitamins Quickly
+                    and Easily To the Confort of Your Own Home.</p>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">Copyright 2021 - Software Engineering II </p>
+        </div>
+    </div>
+</body>
+</html>
