f923e2f9448f78e2cc2a586848a2e7acbdf46814
b-canas <78327376+b-canas@users.noreply.github.com>
2021-04-12 17:46:06 -0400

    Add banner files via upload
    Isolated the top navigation bar Walter had designed in his index.html file and named it "banner". This allows us to easily include the banner into all other webpages. Vitimin admin login has a unique banner, thus a separate file.

---
+++ Vitamin Project/vitimins_worker_banner.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="header">
+    <script type="text/javascript" src="vitimins_script.js"></script>
+
+    <div>
+        <div class="container">
+            <div class="navbar">
+                <div class="logo">
+                    <!-- LINE FOR FUTURE LOGO <img src="images/ " width="125px"> -->
+                </div>
+
+                <nav class="banner">
+                    <ul id="MenuItems">
+                        <li><a href="?action=show_products">Products</a></li>
+                        <li><a href="">Users</a></li>
+                        <li><a href="">Orders</a></li>
+                        <li><a href="">Account Management</a></li>
+                        <li><a href="">Log Off</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
