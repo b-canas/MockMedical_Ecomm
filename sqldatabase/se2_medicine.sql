de2a2e3921534d26e68b383515808b65b11e66a7
Void-Star-Coding <62954238+Void-Star-Coding@users.noreply.github.com>
2021-04-22 05:41:53 -0400

    Updated se2_medicine.sql
    +Added foreign keys and primary keys with cascade directives
    +Added access levels: -1 is customer, 0 is admin, as access increases, rank decreases
    +Added user: "vitadmin" password: "minerals" access: 0, and user: "customer" password: "password" access: -1
    +Added comments for all columns

--- sqldatabase/se2_medicine.sql
+++ sqldatabase/se2_medicine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2021 at 09:29 PM
+-- Generation Time: Apr 22, 2021 at 11:38 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `carts` (
   `USERID` int(16) NOT NULL COMMENT 'FK USERID from users',
   `PRODUCTID` int(16) NOT NULL COMMENT 'FK PRODUCTID from products',
-  `AMOUNT` int(16) NOT NULL COMMENT 'Number of items with the same id ordered - should be truncated everytime called'
+  `AMOUNT` int(16) NOT NULL COMMENT 'Number of items with the same id ordered - must be truncated everytime called'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -40,12 +40,12 @@ CREATE TABLE `carts` (
 --
 
 CREATE TABLE `orderno` (
-  `ORDERNO` int(16) NOT NULL,
+  `ORDERNO` int(16) NOT NULL COMMENT 'Autoincremented - id of order of which "orders" are all derived and grouped by',
   `USERID` int(16) NOT NULL COMMENT 'FK users who initiated the order',
   `TOTALCOST` float NOT NULL COMMENT 'Total cost of the order',
-  `ATTIME` date NOT NULL DEFAULT current_timestamp(),
-  `STATUS` varchar(16) NOT NULL,
-  `LASTMOD` date NOT NULL DEFAULT current_timestamp()
+  `ATTIME` date NOT NULL DEFAULT current_timestamp() COMMENT 'Time the order was originally made',
+  `STATUS` varchar(16) NOT NULL COMMENT 'Status of the order to be updated manually',
+  `LASTMOD` date NOT NULL DEFAULT current_timestamp() COMMENT 'Last time the order was updated'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -55,12 +55,12 @@ CREATE TABLE `orderno` (
 --
 
 CREATE TABLE `orders` (
-  `ORDERNO` int(16) NOT NULL,
-  `USERID` int(16) NOT NULL,
-  `PRODUCTID` int(16) NOT NULL,
-  `AMOUNT` int(16) NOT NULL,
-  `ICOST` float NOT NULL,
-  `TPRICE` float NOT NULL
+  `ORDERNO` int(16) NOT NULL COMMENT 'FK orderno.ORDERNO - this is the order number this/these item(s) are grouped with',
+  `USERID` int(16) NOT NULL COMMENT 'FK users.USERID',
+  `PRODUCTID` int(16) NOT NULL COMMENT 'FK products.PRODUCTID',
+  `AMOUNT` int(16) NOT NULL COMMENT 'Number of items with the same PRODUCTID in this order',
+  `ICOST` float NOT NULL COMMENT 'Individual Cost of one single item with this PRODUCTID',
+  `TPRICE` float NOT NULL COMMENT 'Total Price of the AMOUNT of items (with the same PRODUCTID) multiplied by ICOST'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Orders are grouped by ORDERNO, prod stacks grouped by pid';
 
 -- --------------------------------------------------------
@@ -77,6 +77,27 @@ CREATE TABLE `products` (
   `PRICE` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`PRODUCTID`, `PNAME`, `CATEGORY`, `PSTOCK`, `PRICE`) VALUES
+(1001, 'Vitamin A', 'Child', 100, 10),
+(1002, 'Vitamin B', 'Child', 100, 15),
+(1003, 'Vitamin C', 'Child', 100, 20),
+(1004, 'Vitamin D', 'Child', 100, 25),
+(1005, 'Vitamin E', 'Child', 100, 30),
+(2001, 'Vitamin F', 'Adult', 100, 10),
+(2002, 'Vitamin G', 'Adult', 100, 15),
+(2003, 'Vitamin H', 'Adult', 100, 20),
+(2004, 'Vitamin I', 'Adult', 100, 25),
+(2005, 'Vitamin J', 'Adult', 100, 30),
+(3001, 'Vitamin K', 'Elderly', 100, 10),
+(3002, 'Vitamin L', 'Elderly', 100, 15),
+(3003, 'Vitamin M', 'Elderly', 100, 20),
+(3004, 'Vitamin N', 'Elderly', 100, 25),
+(3005, 'Vitamin O', 'Elderly', 100, 30);
+
 -- --------------------------------------------------------
 
 --
@@ -84,21 +105,50 @@ CREATE TABLE `products` (
 --
 
 CREATE TABLE `users` (
-  `USERID` int(16) NOT NULL,
-  `USERNAME` varchar(16) NOT NULL,
-  `PASSWORD` varchar(16) NOT NULL,
-  `ACCESS` int(16) NOT NULL
+  `USERID` int(16) NOT NULL COMMENT 'Autoincremented PK that indicates this user across all tables',
+  `USERNAME` varchar(16) NOT NULL COMMENT 'Name of user, could be volatile',
+  `PASSWORD` varchar(16) NOT NULL COMMENT 'Password of user, must be encrypted?',
+  `ACCESS` int(16) NOT NULL COMMENT 'Access level of "user" - -1 means customer, 0 is highest level admin, increase value means lesser access',
+  `FNAME` varchar(64) NOT NULL COMMENT 'First name of user',
+  `LNAME` varchar(64) NOT NULL COMMENT 'Last name of user',
+  `ADDRESS` varchar(256) NOT NULL COMMENT 'Address of User - usually the full address for ease',
+  `PHONE` varchar(16) NOT NULL COMMENT 'phone number - string is acceptable because whatever',
+  `EMAIL` varchar(128) NOT NULL COMMENT 'email of user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`USERID`, `USERNAME`, `PASSWORD`, `ACCESS`, `FNAME`, `LNAME`, `ADDRESS`, `PHONE`, `EMAIL`) VALUES
+(1, 'vitadmin', 'minerals', 0, '', '', '', '', ''),
+(2, 'customer', 'password', -1, '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`USERID`),
+  ADD KEY `RelCartsToProductid` (`PRODUCTID`);
+
+--
 -- Indexes for table `orderno`
 --
 ALTER TABLE `orderno`
-  ADD PRIMARY KEY (`ORDERNO`);
+  ADD PRIMARY KEY (`ORDERNO`),
+  ADD KEY `RelOrdernoToUserid` (`USERID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD KEY `RelOrdersToOrderno` (`ORDERNO`),
+  ADD KEY `RelOrdersToProductid` (`PRODUCTID`),
+  ADD KEY `RelOrdersToUserid` (`USERID`);
 
 --
 -- Indexes for table `products`
@@ -120,19 +170,38 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `orderno`
 --
 ALTER TABLE `orderno`
-  MODIFY `ORDERNO` int(16) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `PRODUCTID` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `ORDERNO` int(16) NOT NULL AUTO_INCREMENT COMMENT 'Autoincremented - id of order of which "orders" are all derived and grouped by';
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `USERID` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `USERID` int(16) NOT NULL AUTO_INCREMENT COMMENT 'Autoincremented PK that indicates this user across all tables', AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `carts`
+--
+ALTER TABLE `carts`
+  ADD CONSTRAINT `RelCartsToProductid` FOREIGN KEY (`PRODUCTID`) REFERENCES `products` (`PRODUCTID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `RelCartsToUserid` FOREIGN KEY (`USERID`) REFERENCES `users` (`USERID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orderno`
+--
+ALTER TABLE `orderno`
+  ADD CONSTRAINT `RelOrdernoToUserid` FOREIGN KEY (`USERID`) REFERENCES `users` (`USERID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `RelOrdersToOrderno` FOREIGN KEY (`ORDERNO`) REFERENCES `orderno` (`ORDERNO`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `RelOrdersToProductid` FOREIGN KEY (`PRODUCTID`) REFERENCES `products` (`PRODUCTID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `RelOrdersToUserid` FOREIGN KEY (`USERID`) REFERENCES `users` (`USERID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
