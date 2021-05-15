152ec349200c9b2ab2d0006852bfbcb9955fad47
b-canas <78327376+b-canas@users.noreply.github.com>
2021-05-14 23:24:11 -0400

    Delete "The Joiful CSS update directory"

--- Vitamin_Project/footer.php
+++
@@ -1,8 +0,0 @@
-	<div class="footer">
-        <p>Our purpose is to distribute vitamins quickly and easily
-	       to the comfort of your own home.</p><br>
-        <hr><br>
-        <p class="copyright">&copy; Copyright 2021 - Software Engineering II </p>
-    </div>
-</body>
-</html>
