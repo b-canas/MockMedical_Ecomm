a4860a91fa4e02f45cea171b45bf8557f9e25b00
jalves77 <60753492+jalves77@users.noreply.github.com>
2021-05-20 00:56:46 -0400

    Add files via upload

--- Vitamin Project/createAccount.php
+++ Vitamin Project/createAccount.php
@@ -25,6 +25,23 @@
 				<label for="confirmPassword"></label>
 				<input type="password" placeholder="Confirm Password" name="confirm_password" id="confirm_password"class="input-box" required><br>
 
+				<label for="first_name"></label>
+				<input type="text" placeholder="First Name" name="first_name" id="first_name"class="input-box" required><br>
+
+				<label for="last_name"></label>
+				<input type="text" placeholder="Last Name" name="last_name" id="last_name"class="input-box" required><br>
+
+				<label for="address"></label>
+				<input type="text" placeholder="Address" name="address" id="address"class="input-box" required><br>
+
+				<label for="phone_number"></label>
+				<input type="tel"  pattern ="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Phone Number  Ex: 957-312-1234" name="phone_number" id="phone_number"class="input-box" required><br>
+
+				<label for="email"></label>
+				<input type="email" placeholder="Email" name="email" id="email"class="input-box" required><br>
+
+				
+
 				<button type="submit" id="registerButton" name="registerButton" style="background-color: 008CBA;">Register</button>
 			</div>
 		</form>
