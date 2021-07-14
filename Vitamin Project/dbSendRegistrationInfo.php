16f31fd6cc5c57da566c8b49c395668cba67489d
b-canas <78327376+b-canas@users.noreply.github.com>
2021-07-14 13:15:29 -0400

    Correcting Version differences
    After merging main branch CSS Nightmare, some extra files were left in main that are no longer needed. This commit will remove those files.

--- Vitamin Project/dbSendRegistrationInfo.php
+++ Vitamin Project/dbSendRegistrationInfo.php
@@ -4,13 +4,6 @@ include 'dbConnection.php';
 if (isset($_POST["registerButton"])) {
     $registrationUsername = $_POST["registrationUsername"];
     $registrationPassword = $_POST["registrationPassword"];
-
-    $first_name = $_POST["first_name"];
-    $last_name = $_POST["last_name"];
-    $address = $_POST["address"];
-    $phone_number = $_POST["phone_number"];
-    $email = $_POST["email"];
-
     $hashedPassword = password_hash($registrationPassword, PASSWORD_DEFAULT);
     $normalAccess = "-1";
 
@@ -20,7 +13,7 @@ if (isset($_POST["registerButton"])) {
         echo "Username already exists! Failed to create account";
     }
     else {
-        $query = "INSERT INTO users (USERNAME,PASSWORD,ACCESS,FNAME,LNAME,ADDRESS,PHONE,EMAIL) VALUES ('$registrationUsername','$hashedPassword','$normalAccess','$first_name','$last_name','$address','$phone_number','$email')";
+        $query = "INSERT INTO users (USERNAME,PASSWORD,ACCESS) VALUES ('$registrationUsername','$hashedPassword','$normalAccess')";
         $run = mysqli_query($conn,$query);
     }
 }
